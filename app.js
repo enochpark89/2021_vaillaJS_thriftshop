09de2d35f285624055e808c7505a63f9c35bf7bf
Enoch Park <44038883+enochpark89@users.noreply.github.com>
2021-07-31 22:10:50 -0700

    20210731

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 // Form HTML - "itemForm"
 const itemForm = document.getElementById("itemForm");
 
-// ul HTML - "storage"
-const storage = document.getElementById("storage");
-
 // input fields ( item and price. )
 const item = document.getElementById("item");
 const price = document.getElementById("price");
@@ -18,23 +15,63 @@ const storageKey = "storage";
 const storeKey = "store";
 const soldKey = "sold";
 
-// function that save to localStorage
+// function that save data to localStorage
 function saveData(key) {
    if (key === storageKey) {
       localStorage.setItem(storageKey, JSON.stringify(storageArray));
    } else if (key === storeKey) {
       localStorage.setItem(storeKey, JSON.stringify(storeArray));
    } else {
-      localStorage.setItem(soldKey, JSON.stringify(storageArray));
+      localStorage.setItem(soldKey, JSON.stringify(soldArray));
    }
 }
 
-// function that move data from the store to sell
+function calculateTotal() {
+   let result = document.getElementById("result");
+   total = 0;
+   soldArray.forEach(element => total+=parseInt(element.price));
 
-function moveToSell() {}
+   result.innerText = " $ " + JSON.stringify(total);
+   
+}
 
+
+// Display data to sold table on the webpage.
+function paintSold(itemObj) {
+
+   let table = document.getElementById("soldTable");
+   let row = table.insertRow(-1);
+   let cell1 = row.insertCell(0);
+   cell1.innerText = itemObj.id;
+   let cell2 = row.insertCell(1);
+   cell2.innerText = itemObj.item;
+   let cell3 = row.insertCell(2);
+   cell3.innerText = "$" + itemObj.price;
+   calculateTotal()
+}
+
+// function that move data from the a store to a sell
+function moveToSell(event) {
+   // get a row and a ID from the store table.
+   const row = event.target.parentElement.parentElement;
+   const id = parseInt(row.firstChild.innerText);
+   row.remove();
+
+   // find the item from the storeArray and move to soldArray and save to the localStorage.
+   const Arr = storeArray.filter((itemObj) => itemObj.id === id);
+   soldArray.push(Arr[0]);
+   paintSold(Arr[0]);
+
+   saveData(soldKey);
+
+   // filter out (or delete) selected item from the storeArray and save to the localStorage.
+   storeArray = storeArray.filter((itemObj) => itemObj.id !== id);
+   saveData(storeKey);
+
+}
+
+// Display data to store table on the webpage.
 function paintStore(itemObj) {
-   // Create a row and move the selected row information from storage section to store section.
 
    let table = document.getElementById("storeTable");
    let row = table.insertRow(-1);
@@ -62,20 +99,21 @@ function moveToStore(event) {
 
    // find the item from the storageArray and move to storeArray and save to the localStorage - store
    const Arr = storageArray.filter((itemObj) => itemObj.id === id);
-   console.log(Arr);
-   console.log(storeArray);
    storeArray.push(Arr[0]);
-   console.log(storeArray);
+   paintStore(Arr[0]);
 
    saveData(storeKey);
 
-   // filter out found item from the storageArray and save to the localStorage - storage
+   // filter out(or delete) a selected item from the storageArray and save to the localStorage.
    storageArray = storageArray.filter((itemObj) => itemObj.id !== id);
    saveData(storageKey);
+
+
 }
 
+// Display data to storage table on the webpage.
 function paintStorage(itemObj) {
-   // Create a row
+   // Create a row for a storage
    let table = document.getElementById("storageTable");
    let row = table.insertRow(-1);
    let cell1 = row.insertCell(0);
@@ -103,12 +141,9 @@ function handleStore(event) {
    const priveVal = parseInt(price.value);
    const itemVal = item.value;
 
-   // clear the values
+   // clear item and price values.
    price.value = "";
    item.value = "";
-
-   //  console.log("you entered the item", item.value, price.value);
-   //  console.log(typeof price.value, typeof itemVal);
 
    const itemObj = {
       id: id,
@@ -122,9 +157,9 @@ function handleStore(event) {
 
 itemForm.addEventListener("submit", handleStore);
 
-// When the page starts, it gets the data from the localStorage.
+// When the page starts, it gets the data from the localStorage and store in the fake db.
+// Then, paint them to the website so that localStorage data will reflect both on the webpadge and arrays(fake DBs)
 
-//
 const savedStorage = localStorage.getItem(storageKey);
 
 if (savedStorage !== null) {
@@ -139,10 +174,10 @@ if (savedStore !== null) {
    storeArray = parsedSavedStore;
    parsedSavedStore.forEach(paintStore);
 }
-// const savedSold = localStorage.getItem(soldKey);
+const savedSold = localStorage.getItem(soldKey);
 
-// if (savedSold !== null) {
-//    const parsedSavedSold = JSON.parse(savedSold);
-//    soldArray = parsedSavedSold;
-//    parsedSavedStorage.forEach(moveToSold);
-// }
+if (savedSold !== null) {
+   const parsedSavedSold = JSON.parse(savedSold);
+   soldArray = parsedSavedSold;
+   parsedSavedSold.forEach(paintSold);
+}
